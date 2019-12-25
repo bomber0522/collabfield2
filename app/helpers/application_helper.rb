1c6699498ef5cdf2eacd27c7d334e54bb11bb055
Takaki <takaki55730317@gmail.com>
2019-12-25 20:59:42 +0900

    Render a private conversation window on the app - Add opened conversations to the session - Create a _conversation.html.erb file inside private/conversaions - Define a private_conv_recipient helper method in the private/conversations_helper.rb - Define an opposed_user method in Private::Conversation model and add specs for it - Create _heading.html.erb and _messages_list.html.erb files inside the private/conversations/conversation - Define a load_private_messages in private/converstions_helper.rb and add specs for it - Create a _new_message_form.html.erb inside the private/conversations/conversation - Create a _open.js.erbinside private/conversations - Create a position_and_visibility.js inside the assets/javascripts/conversations - Create a conversations_window.scss inside the assets/stylesheets/partials - Define an opened_conversations_windows helper method in ApplicationController - Create a _private_conversations_windows.html.erb inside the layouts/application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ require 'navigation_helper.rb'
 module ApplicationHelper
   include NavigationHelper
   include PostsHelper
+  include Private::ConversationsHelper
+
+  def private_conversations_windows
+    params[:controller] != 'messengers' ? @private_conversations_windows : []
+  end
 end
