5784298aca362f81fffc7e5011bcb569642bd9d7
Takaki <takaki55730317@gmail.com>
2020-01-03 21:00:59 +0900

    Define a set_user_data private method in ApplecationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :opened_conversations_windows
   before_action :all_ordered_conversations
+  before_action :set_user_data
 
   def all_ordered_conversations
     if user_signed_in?
@@ -40,6 +41,19 @@ class ApplicationController < ActionController::Base
     else
       @private_conversations_windows = []
       @group_conversations_windows = []
+    end
+  end
+
+  private
+
+  def set_user_data
+    if user_signed_in?
+      gon.group_conversations = current_user.group_conversations.ids
+      gon.user_id = current_user.id
+      cookies[:user_id] = current_user.id if current_user.present?
+      cookies[:group_conversations] = current_user.group_conversations.ids
+    else
+      gon.group_conversations = []
     end
   end
 end
