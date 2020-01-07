c9e7ce9842ca7f5c547f3378b0ece89c770c0415
Takaki <takaki55730317@gmail.com>
2020-01-07 19:18:00 +0900

    Define an autoload_messenger_messages in the Shared::MessagesHelper

--- app/helpers/shared/messages_helper.rb
+++ app/helpers/shared/messages_helper.rb
@@ -16,4 +16,19 @@ module Shared::MessagesHelper
   def append_previous_messages_partial_path
     'shared/load_more_messages/window/append_messages'
   end
+
+  # in the messenger load previous messages until the scroll bar appears
+  def autoload_messenger_messages
+    if @is_messenger == 'true'
+      # if previous messages exist, load them
+      if @messages_to_display_offset != 0
+        'shared/load_more_messages/messenger/load_previous_messages'
+      else
+        # remove load previous messages link
+        'shared/load_more_messages/messenger/remove_previous_messages_link'
+      end
+    else
+      'shared/empty_partial'
+    end
+  end
 end
