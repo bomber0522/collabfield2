916ef514770b5e9f30b9f39183f652c5ad1c4e96
Takaki <takaki55730317@gmail.com>
2020-01-11 21:17:26 +0900

    Deleted spec/helpers/group/conversations/_messages.html.erb - Modified spec/helpers/group/conversations_helper_spec.rb - Deleted spec/helpers/group/messages/_new_date.html.erb - Deleted spec/helpers/group/messages/message/_different_user_content.html.erb - Deleted spec/helpers/group/messages/message/_same_user_content.html.erb - Modified spec/helpers/group/messages_helper_spec.rb - Deleted  spec/helpers/private/conversations/window_spec.rb

--- spec/helpers/group/conversations_helper_spec.rb
+++ spec/helpers/group/conversations_helper_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe Group::ConversationsHelper, type: :helper do
     let(:current_user) { create(:user) }
     let(:user) { create(:user) }
     before(:each) do
-      create(:contact,
-              user_id: current_user.id,
+      create(:contact, 
+              user_id: current_user.id, 
               contact_id: user.id,
               accepted: true)
     end
@@ -18,17 +18,17 @@ RSpec.describe Group::ConversationsHelper, type: :helper do
       helper.stub(:current_user).and_return(current_user)
       expect(helper.add_people_to_group_conv_list(conversation)).not_to include user
     end
-  
+
     it 'a user is included in a list' do
       conversation.users << current_user
-      helpler.stub(:current_user).and_return(current_user)
+      helper.stub(:current_user).and_return(current_user)
       expect(helper.add_people_to_group_conv_list(conversation)).to include user
     end
   end
 
   context '#load_group_messages_partial_path' do
     let(:conversation) { create(:group_conversation) }
-    it "returns load_messages partila's path" do
+    it "returns load_messages partial's path" do
       create_list(:group_message, 2, conversation_id: conversation.id)
       expect(helper.load_group_messages_partial_path(conversation)).to eq(
         'group/conversations/conversation/messages_list/link_to_previous_messages'
@@ -36,7 +36,7 @@ RSpec.describe Group::ConversationsHelper, type: :helper do
     end
 
     it "returns an empty partial's path" do
-      expect(helper.load_group_messages_partial_path(covnersation)).to eq(
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
         'shared/empty_partial'
       )
     end
