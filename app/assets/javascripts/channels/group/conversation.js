a99346aff3f1a57ef2b60cf830e2fc17916de3cf
Takaki <takaki55730317@gmail.com>
2020-01-11 22:16:58 +0900

    Modified app/assets/javascripts/channels/group/conversation.js - Modified app/assets/javascripts/channels/notification.js - Modified app/assets/javascripts/channels/notification.js - Modified app/assets/javascripts/channels/shared/conversation.js - Modified app/assets/javascripts/conversations/conversation.js - Modified app/assets/javascripts/conversations/messages_infinite_scroll.js - Modified app/assets/javascripts/conversations/options.js - Modified app/assets/javascripts/conversations/position_and_visibility.js - Modified app/assets/javascripts/conversations/toggle_window.js

--- app/assets/javascripts/channels/group/conversation.js
+++ app/assets/javascripts/channels/group/conversation.js
@@ -1,55 +1,55 @@
 for (i = 0; i < gon.group_conversations.length; i++) {
-  subToGroupConversationChannel(gon.group_conversatons[i]);
+  subToGroupConversationChannel(gon.group_conversations[i]);
 }
 
 function subToGroupConversationChannel(id) {
-  App['group_conversation_' + id] = App.cable.subscriptions.create(
-    {
-      channel: 'Group::ConversationChannel',
-      id: id
-    },
-    {
-      connected: function() {},
-      disconnected: function() {},
-      received: function(data) {
-        console.log('swap');
-        // prepend link to the conversation
-        // to the top of conversations menu list
-        modifyConversationsMenuList(data['conversation_id']);
-
-        // set varivables
-        var conversation = findConv(data['conversation_id'], 'g');
-        var conversation_rendered = ConvRendered(data['conversation_id'], 'g');
-        var messages_visible = ConvMessagesVisiblity(conversation);
-
-        // if the message is not sent by the user,
-        // mark the conversation as unseen
-        MarkGroupConvAsUnseen(data['user_id'], data['conversation_id']);
-
-        // append the new message
-        appendGroupMessage(conversation_rendered,
-                           messages_visible,
-                           data['message']);
-
-        // if the converstion window is rendered
-        if (conversation_rendered) {
-          // after the new message was appended
-          // scroll to the bottom of the conversation window
-          var messages_list = conversation.find('.messages-list');
-          var height = messages_list[0].scrollHeight;
-          messages_list.scrollTop(height);
-        }
-
+  App['group_conversation_' + id]  = App.cable.subscriptions.create(
+      {
+          channel: 'Group::ConversationChannel',
+          id: id
       },
-      send_message: function(message) {
-        return this.perform('send_message', {
-          message: message
-        });
-      },
-      set_as_seen: function(conv_id) {
-        return this.perform('set_as_seen', { conv_id: conv_id });
+      {
+          connected: function() {},
+          disconnected: function() {},
+          received: function(data) {
+              // prepend link to the conversation 
+              // to the top of conversations menu list
+              modifyConversationsMenuList(data['conversation_id']);
+
+              // set variables
+              var conversation = findConv(data['conversation_id'], 'g');
+              var conversation_rendered = ConvRendered(data['conversation_id'], 'g');
+              var messages_visible = ConvMessagesVisiblity(conversation);
+
+              // if the message is not sent by the user, 
+              // mark the conversation as unseen
+              MarkGroupConvAsUnseen(data['user_id'], data['conversation_id']);
+
+              // append the new message
+              appendGroupMessage(conversation_rendered, 
+                                 messages_visible, 
+                                 conversation,
+                                 data['message']);
+
+              // if the conversation window is rendered
+              if (conversation_rendered) {
+                  // after the new message was appended 
+                  // scroll to the bottom of the conversation window
+                  var messages_list = conversation.find('.messages-list');
+                  var height = messages_list[0].scrollHeight;
+                  messages_list.scrollTop(height);
+              }
+              
+          },
+          send_message: function(message) {
+              return this.perform('send_message', {
+                  message: message
+              });
+          },
+          set_as_seen: function(conv_id) {
+              return this.perform('set_as_seen', { conv_id: conv_id });
+          }
       }
-    }
   );
 }
 
@@ -67,73 +67,75 @@ $(document).on('click', '.conversation-window, .group-conversation', function(e)
   var unseen_by_user = latest_message.hasClass('unseen');
   // if not seen by the user
   if (unseen_by_user) {
-    var conv_id = $(this).find('.panel').attr('data-gconversation-id');
-    // if conv_id dosen't exist, it means that message was seen in messenger
-    if (conv_id == null) {
-      var conv_id = $(this).find('.messages-list').attr('data-gconversation-id');
-    }
-    // mark conversation as seen in conversations menu list
-    $('#menu-gc' + conv_id).removeClass('unseen-conv');
-    latest_message.removeClass('unseen');
-    calculateUnseenConversations();
-    App['group_conversation_' + conv_id].set_as_seen(conv_id);
+      var conv_id = $(this).find('.panel').attr('data-gconversation-id');
+      // if conv_id doesn't exist, it means that message was seen in messenger
+      if (conv_id == null) {
+          var conv_id = $(this).find('.messages-list').attr('data-gconversation-id');
+      }
+      // mark conversation as seen in conversations menu list
+      $('#menu-gc' + conv_id).removeClass('unseen-conv');
+      latest_message.removeClass('unseen');
+      calculateUnseenConversations();
+      App['group_conversation_' + conv_id].set_as_seen(conv_id);
   }
 });
 
 function MarkGroupConvAsUnseen(message_user_id, conversation_id) {
-  // if the message is not sent by the user, mark the conversation as unseen
+   // if the message is not sent by the user, mark the conversation as unseen
   if (message_user_id != gon.user_id) {
-    newGroupConvMenuListLink(conversation_id);
+      newGroupConvMenuListLink(conversation_id);
 
-    // mark the conversation as unseen, after the new messge is received
-    $('#menu-gc' + conversation_id).addClass('unseen-conv');
-    calculateUnseenConversations();
+      // mark the conversation as unseen, after the new message is received
+      $('#menu-gc' + conversation_id).addClass('unseen-conv');
+      calculateUnseenConversations();
   }
+                
 }
 
-// prepend link to the conversatin to the top of conversations menu list
+// prepend link to the conversation to the top of conversations menu list
 function modifyConversationsMenuList(conversation_id) {
   // if the conversation link in conversations menu list exists
   // move conversation link to the top of the conversations menu list
   var conversation_menu_link = $('#conversations-menu ul')
                                    .find('#menu-gc' + conversation_id);
   if (conversation_menu_link.length) {
-    conversation_menu_link.prependTo('#conversations-menu ul');
+      conversation_menu_link.prependTo('#conversations-menu ul');
   }
 }
 
 // append the new message to the list
-function appendGroupMessage(conversation_rendered,
-                            messages_visible,
-                            group_conversation,
-                            message) {
+function appendGroupMessage(conversation_rendered, 
+                          messages_visible, 
+                          group_conversation,
+                          message) {
   if (conversation_rendered) {
-    // if the conversation is collapsed
-    if (!messsages_visible) {
-      // mark its header color
-    }
-    // append the new message to the list
-    group_conversation
-    .find('.messages-list')
-    .find('ul')
-    .append(message);
+      // if the conversation is collapsed
+      if (!messages_visible) {
+          // mark its header color
+      }
+      // append the new message to the list
+      group_conversation
+          .find('.messages-list')
+          .find('ul')
+          .append(message);
   }
+
 }
 
-// if the conversatin link in the conversations menu list doesn't exist
+// if the conversation link in the conversations menu list doesn't exist
 // create a new link with the receiver's name and prepend it to the list
 function newGroupConvMenuListLink(conversation_id) {
   var id_attr = '#menu-gc' + conversation_id;
   var conversation_menu_link = $('#conversations-menu ul').find(id_attr);
   if (conversation_menu_link.length == 0) {
-    var list_item = '<li class="conversation-window">\
-                         <a data-remote="true"\
-                            rel="nofollow"\
-                            data-method="post"\
-                            href="/group_conversations?group_conversation_id=' + conversation_id + '">\
-                                group conversation\
-                         </a>\
-                     </li>';
-    $('#conversations-menu ul').prepend(list_item);
+      var list_item = '<li class="conversation-window">\
+                           <a data-remote="true"\
+                              rel="nofollow"\
+                              data-method="post"\
+                              href="/group_conversations?group_conversation_id=' +  conversation_id + '">\
+                                  group conversation\
+                           </a>\
+                       </li>';
+      $('#conversations-menu ul').prepend(list_item);
   }
 }
